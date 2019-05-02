29b63bf8c31ad629edc9d841c85438a71fbbf30a
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-05-02 17:09:34 -0400

    still working logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,42 +5,65 @@ var friends = require("../data/friend");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app){
+module.exports = function (app) {
 
     //update to the api friends
-    app.get("/api/survey", (req, res) => {
-        res.json(friends);
-
-    });
-
-    app.get("/results", (req, res) => {
+    app.get("/api/survey", function(req, res) {
         res.json(friends);
     });
 
-    app.get("/matches", (req, res) => {
-        res.json(friends);
-    });
-//==================================================================
- 
+    // app.get("/results", (req, res) => {
+    //     res.json(friends);
+    // });
+
+    //==================================================================
+
     //get users content
-    app.post("/api/survey", (req, res) => {
-        //Store new user info
+    app.post("/api/survey", function(req, res) {
+        //Store new user inf
         var newFriend = req.body;
-        //compare new user to friend database
+        var difference = 0;
+        var matchingFriend;
+        // compare new user to friend database
         for (let i = 0; i < friends.length; i++) {
+            var match = friends[i];
+            var bestMatch = 30;
             // test each user's score against newFriend.
-            for (let j = 0; j < newFriend.scores.length; j++) {
-                var difference = 0;
-                var totalDifference = [];
-                var match  = []
-                difference = Math.abs(newFriend.scores - friends.scores[j]);
-                
-                
+            for (let j = 0; j < 10; j++) {
+                var differenceTotal =0
+                var differenceNumber = 0;
+              
+                difference = Math.abs(newFriend.scores[j] - friends[i].scores[j]);
+                differenceTotal += difference;
             }
+
+            differenceNumber = differenceTotal;
+
+            if (bestMatch > differenceNumber){
+                bestMatch = differenceNumber;
+                matchingFriend = match.name;
+                console.log("Best Match " + bestMatch);
+            }
+
+
         }
-        res.json();
+
+        
+        console.log("BEST MATCH NUMBER is " + bestMatch + " and that person is " + matchingFriend);
+
         //push new user to the friend database
-        friends.push(req.body);
-        console.log(`ADDED TO POST`);
+        // friends.push(newFriend);
+        // // res.json(friends);
+        // res.json(true);
+        // console.log(friends);
+        // console.log(`ADDED TO POST`);
+
+        // ----------------
+    
+            friends.push(req.body);
+
+            console.log(friends);
+            res.json(friends);
+        // console.log(newFriend);
     })
 }
--- server.js
+++ server.js
@@ -11,14 +11,18 @@ require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
 
 
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-  });
+// app.get("/survey", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/survey.html"));
+//   });
 
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
+//   // If no matching route is found default to home
+//   app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/home.html"));
+//   });
+
+//   app.get("/results", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/results.html"));
+//   });
 
 app.listen(PORT, function(){
     console.log("App listenting on PORT: "+ PORT);
