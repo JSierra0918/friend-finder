4da1f26e32effa867acc577fc752160a17ce218a
Jorge <3d.jorgesierra@gmail.com>
2019-05-03 17:45:42 -0400

    changed some variables

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,7 @@ var friends = require("../data/friend");
 module.exports = function (app) {
 
     //update to the api friends
-    app.get("/api/survey", function(req, res) {
+    app.get("/api/survey", function (req, res) {
         res.json(friends);
     });
 
@@ -19,51 +19,37 @@ module.exports = function (app) {
     //==================================================================
 
     //get users content
-    app.post("/api/survey", function(req, res) {
+    app.post("/api/survey", function (req, res) {
         //Store new user inf
         var newFriend = req.body;
         var difference = 0;
         var matchingFriend;
+
         // compare new user to friend database
         for (let i = 0; i < friends.length; i++) {
             var match = friends[i];
             var bestMatch = 30;
             // test each user's score against newFriend.
             for (let j = 0; j < 10; j++) {
-                var differenceTotal =0
+                var differenceTotal = 0
                 var differenceNumber = 0;
-              
-                difference = Math.abs(newFriend.scores[j] - friends[i].scores[j]);
+
+                difference = Math.abs(newFriend.scores[j] - match.scores[j]);
                 differenceTotal += difference;
             }
 
-            differenceNumber = differenceTotal;
+            // differenceNumber = differenceTotal;
 
-            if (bestMatch > differenceNumber){
-                bestMatch = differenceNumber;
+            if (bestMatch > differenceTotal) {
+                bestMatch = differenceTotal;
                 matchingFriend = match.name;
                 console.log("Best Match " + bestMatch);
             }
-
-
         }
-
-        
-        console.log("BEST MATCH NUMBER is " + bestMatch + " and that person is " + matchingFriend);
-
-        //push new user to the friend database
-        // friends.push(newFriend);
-        // // res.json(friends);
-        // res.json(true);
-        // console.log(friends);
-        // console.log(`ADDED TO POST`);
-
-        // ----------------
-    
-            friends.push(req.body);
-
-            console.log(friends);
-            res.json(friends);
-        // console.log(newFriend);
+        // push the new user to friend.
+       console.log("BEST MATCH NUMBER is " + bestMatch + " and that person is " + matchingFriend);
+        friends.push(req.body);
+        console.log(friends);
+        res.json(friends);
     })
 }
